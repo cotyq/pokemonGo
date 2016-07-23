fdefedc34e03b4e663e5f39f870200bc945bd749
cotyq <cotii.q@gmail.com>
2016-07-23 15:12:04 -0300

    vistas mapa y formulario

---
+++ resources/views/create.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html ng-app="pokemonGo">
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    
+
+    <!-- Bootstrap latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	</head>
+    <body>
+     
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Nombre</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+      </div>
+
+      <div class="form-group">
+        <label for="exampleInputFile">Imagen</label>
+        <input type="file" id="exampleInputFile">
+        <p class="help-block">Example block-level help text here.</p>
+      </div>
+
+      <button type="submit" class="btn btn-default">Finalizar</button>
+
+    </form>
+
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script async src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular.min.js"></script>  
+
+    <!-- Bootstrap -->
+    <script async src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+    </script>
+ 
+   </body>
+</html>
